14bef0688b58ffe10d9a596775c8116a856a38f9
zahra0ghorbani <zizi63806380@gmail.com>
2025-03-26 13:47:31 +0330

    Main_PM_1_2

---
+++ ap/exercises/ex2/Main_PM_1_2.java
@@ -0,0 +1,28 @@
+package ap.exercises.ex2;
+
+import java.util.Scanner;
+
+public class Main_PM_1_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the k : ");
+        int k = scanner.nextInt();
+        scanner.close();
+        char [][] square = new char[k+2][k+2];
+        for (int i = 0; i < k+2; i++) {
+            for (int j = 0; j < k+2; j++) {
+                if (i == 0 || i == k+1 || j == 0 || j == k+1) {
+                    square[i][j] = '*';
+                }else{
+                    square[i][j] = ' ';
+                }
+            }
+        }
+        for (int i = 0; i < k+2; i++) {
+            for (int j = 0; j < k+2; j++) {
+                System.out.print(square[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
